cbf082ef0367e93137a42caa3ec2fa1bc0d3b8a1
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-25 12:57:35 -0700

    $commit_title

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
