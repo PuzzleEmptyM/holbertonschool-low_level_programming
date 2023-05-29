c29b84f692bb10270f77795eb573539d3ba3b0e9
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 07:42:15 -0700

    $commit_title

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,16 +10,19 @@ int main(void)
 {
 	int n = 48;
 	int l = 'a';
+
 	while (n <= 57)
 	{
 		putchar(n);
 		n += 1;
 	}
+
 	while (l <= 'f')
 	{
 		putchar(l);
 		l++;
 	}
+
 	putchar('\n');
 	return (0);
 }
