c2ceafdd51bf8b0e9b9ee8356da937b918300c95
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 09:45:57 -0700

    $commit_title

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,16 @@
 #include <main.h>
 #include <main.h>
 
-void print_alphabet(void); /* Function prototype for print_alphabet */
-
-int main(void)
-{
-	print_alphabet();
-	return 0;
-}
-
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
-	{
 		_putchar(i);
-	}
 	_putchar('\n');
 }
