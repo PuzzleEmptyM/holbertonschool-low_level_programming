f2d67377be86dd55321cc97198160e76206299fa
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 10:34:59 -0700

    $commit_title

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,12 @@
-int _putchar(char);
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+/* Wheeeee prototypes */
+
+int _putchar(char c);
+void print_alphabet(void);
 void times_table(void);
+#endif
