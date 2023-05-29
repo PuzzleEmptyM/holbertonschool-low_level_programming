7084fe405848b22c77c3265d8d00f61b32743f5e
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 08:29:57 -0700

    $commit_title

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Calls the function _putchar to print with
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	char a[] = "_putchar";
+	
+	int s = sizeof(a);
+	
+	int b;
+	
+	for (b = 0; b < (s - 1); b++)
+	
+	{
+	
+		_putchar(a[b]);
+	
+	}
+	
+	_putchar('\n');
+	
+	return (0);
+
+}
