1c0e3a56deb7731f59e6b521391d92801565e775
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 07:55:23 -0700

    $commit_title

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,19 @@ int main(void)
 	
 	int lw = 'a';
 	int up = 'A';
-	
+
 	while (lw <= 'z')
 	{
 		putchar(lw);
 		lw += 1;
 	}
-	
+
 	while (up <= 'Z')
 	{
 		putchar(up);
 		up += 1;
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
