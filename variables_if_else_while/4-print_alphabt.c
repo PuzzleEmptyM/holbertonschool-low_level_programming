d5b16709d391015a4ecb1296c8f99c6ac740415a
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-29 07:58:04 -0700

    $commit_title

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,33 @@
  */
 
 int main(void)
+
 {
+
 	int lw = 'a';
+
 	while (lw <= 'z')
+
 	{
+
 		if (lw == 'e' || lw == 'q')
+
 		{
+
 			lw += 1;
+
 		}
+
 		else
+
 		{
+
 			putchar(lw);
 			lw += 1;
+
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
