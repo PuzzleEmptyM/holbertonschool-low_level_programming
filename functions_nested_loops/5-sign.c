15a5a4065d88db07cfd2e9d73579ca8bbfe41a92
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-05-30 12:26:22 -0700

    $commit_title

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		return (1);
+		return (-1);
 	}
 	else if (n > 0)
 	{
@@ -21,7 +21,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar(43);
+		_putchar(48);
 		return (0);
 	}
 
