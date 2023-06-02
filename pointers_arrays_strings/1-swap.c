c438656296507d022b2f7cd277dec1e2acb00438
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-02 13:39:16 -0700

    $commit_title

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * swap_int - swaps the values of the two integers.
+ *
+ * @a: pointer to an int
+ * @b: pointer to the other int
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
