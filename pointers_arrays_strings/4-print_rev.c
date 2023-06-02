d1c74c44c9105e8cf5391078f6cecfc25ae93a21
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-02 14:23:36 -0700

    $commit_title

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - function that prints a string, in reverse order
+ * @s: String to print
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + 1) != '\0')
+		i += 1;
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
