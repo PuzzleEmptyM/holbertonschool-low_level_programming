7648199622aa20e5a8e57d6f475aedeb1bd9ba84
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-12 20:48:56 -0700

    $commit_title

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - function that changes lowercase letters to upper
+ *
+ * @ch: pointer to char
+ * Return: char
+ */
+
+char *string_toupper(char *ch)
+{
+	int i = 0;
+
+	while (*(ch + i) != '\0')
+	{
+		if (*(ch + i) >= 97 && *(ch + i) <= 122)
+			*(ch + i) = *(ch + i) - ' ';
+		i++;
+	}
+	return (ch);
+}
