4aef6732179d3d24faefee91b70828e55017093f
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-12 20:38:03 -0700

    $commit_title

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*(s1 + i) - *(s2 + i))
 		{
-			aux = *(s1 + i) - *(s2 = i);
+			aux = *(s1 + i) - *(s2 + i);
 			return (aux);
 		}
 		else if (*(s1 + i) < *(s2 + i))
