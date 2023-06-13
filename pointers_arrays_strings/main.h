ce0fec85cdb2a1a0c9ae1d9e848a14f6fcfcf9ff
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-12 20:20:56 -0700

    $commit_title

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,5 +18,13 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
 
 #endif
