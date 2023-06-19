9bfa227cebf9399219147911581b7eb8cefba996
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-06-19 11:54:24 -0700

    $commit_title

---
+++ static_libraries/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src
