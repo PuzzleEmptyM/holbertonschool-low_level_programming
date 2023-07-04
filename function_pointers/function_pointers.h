9d0ee1b337feb05f2e4682a0fe8b22253ffe24cd
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-04 12:05:42 -0700

    fixed heade

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-typedef __SIZE_TYPE__ size;
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif /* #ifndef HOLBERTON_H */
+#endif /* #ifndef FUNC_POINTERS */
