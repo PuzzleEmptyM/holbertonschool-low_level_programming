cbff5955efbd13c7708a3053029ff9875e7832df
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-07-17 20:41:26 -0700

    file 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 	/* if text, writes text into file */
 	if (text_content)
 	{
-		for (len = 0; text_content[len]; len++);
+		for (len = 0; text_content[len]; len++)
+			;
 		check = write(file, text_content, len);
 	}
 	/* closes file */
