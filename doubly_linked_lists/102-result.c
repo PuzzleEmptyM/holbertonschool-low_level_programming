8deddc2c084a4b4dd0025359e18351f802894e61
PuzzleEmptyM <emptympuzzle@gmail.com>
2023-08-14 15:31:06 -0700

    test

---
+++ doubly_linked_lists/102-result.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * this is a function that finds the largest palindrome made
+ * from the product of two 3-digit numbers.
+ * @s: the string of integers of our palindrome
+ * @start: the start of the string
+ * @mid: the middle of the string
+ * @end: the end of the string
+ *
+ * Return: not applicable
+ */
+
+int palindrome_results(const char *s)
+{
+	/**int start, mid, end = */;
+	char result = "906609";
+
+
+	return (echo "$result" > 102-result);
+}
